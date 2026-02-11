bbd179f406983f9bec178cabda84e028a94dcd8a
Jennifer Tyler <83067625+chickenjenders@users.noreply.github.com>
2026-02-10 22:24:47 -0500

    Refactor MiseryManager to integrate with ShiftManager; remove unused trait options loading and update scene transitions

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/desktop.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
