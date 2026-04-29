56a5b31f071774fcf9e3911a13daf85ceb579540
Jennifer Tyler <83067625+chickenjenders@users.noreply.github.com>
2026-04-29 19:11:56 -0400

    Add new assets and update intro dialogue: include images, import files, and enhance dialogue content

--- Core/Scripts/introvideo.gd
+++ Core/Scripts/introvideo.gd
@@ -34,8 +34,29 @@ func _ready():
 		start_dialogue()
 
 func load_slides():
-	for i in range(2, 23):
-		var path = "res://assets/introslide/%d.png" % i
+	var slide_paths = [
+		"res://assets/introslide/2.png",
+		"res://assets/introslide/3.png",
+		"res://assets/introslide/4.png",
+		"res://assets/introslide/5.png",
+		"res://assets/introslide/6.png",
+		"res://assets/introslide/7.png",
+		"res://assets/introslide/8.png",
+		"res://assets/introslide/9.png",
+		"res://assets/introslide/101112.png",
+		"res://assets/introslide/13.png",
+		"res://assets/introslide/14.png",
+		"res://assets/introslide/15.png",
+		"res://assets/introslide/16.png",
+		"res://assets/introslide/17.png",
+		"res://assets/introslide/18.png",
+		"res://assets/introslide/19.png",
+		"res://assets/introslide/20.png",
+		"res://assets/introslide/21.png",
+		"res://assets/introslide/22.png"
+	]
+
+	for path in slide_paths:
 		if ResourceLoader.exists(path):
 			slides.append(load(path))
 		else:
@@ -54,6 +75,7 @@ func start_dialogue():
 func _on_got_dialogue(_line):
 	current_slide_index += 1
 	set_slide(current_slide_index)
+	set_intro_dialogue_box_transparent(current_slide_index == 13)
 	
 	if current_slide_index == 2:
 		AudioManager.play_music(bgmusic)
@@ -61,15 +83,24 @@ func _on_got_dialogue(_line):
 		AudioManager.play_music(laugh)
 	elif current_slide_index == 7:
 		AudioManager.play_music(bgmusic)
-	elif current_slide_index == 15:
+	elif current_slide_index == 13:
 		AudioManager.play_music(scaryinst)
-	elif current_slide_index == 16:
+	elif current_slide_index == 14:
 		AudioManager.play_music(bgmusic)
+
+func set_intro_dialogue_box_transparent(is_transparent: bool):
+	var balloon_layer = get_tree().root.find_child("ExampleBalloon", true, false)
+	if balloon_layer == null:
+		return
+
+	var panel_container = balloon_layer.get_node_or_null("Balloon/MarginContainer/PanelContainer")
+	if panel_container:
+		panel_container.visible = not is_transparent
 
 func set_slide(index: int):
 	if index > 0 and index <= slides.size():
 		intro_texture.texture = slides[index - 1]
-		if index == 1 or index == 15:
+		if index == 1 or index == 13:
 			intro_texture.set_anchors_preset(Control.PRESET_FULL_RECT)
 			intro_texture.offset_left = 0.0
 			intro_texture.offset_top = 0.0
